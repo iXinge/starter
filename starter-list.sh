4100ba82e9b937cc05b33cdb292f1c804a2df94a
Shawn Wang <ixingo@126.com>
2021-01-12 23:58:09 +0800

    Update starter-list.sh

--- starter-list.sh
+++ starter-list.sh
@@ -4,6 +4,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 sudo chsh -s $(which zsh)
 
+
 sudo apt-get install python3-dev python3-pip libmysqlclient-dev
 sudo apt-get install openjdk-8-jdk
 sudo apt-get install mysql-server
@@ -16,12 +17,16 @@ FLUSH PRIVILEGES;
 -- GRANT privileges ON *.* TO 'dev'@'localhost';
 )
 
+
 sudo apt-get install redis
 sudo apt-get install maven
 sudo apt-get install node
 sudo apt-get install mongo
+
+
 ssh-keygen -t rsa -b 4096 -C "ixingo@126.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
+
 git config --global user.name "Shawn Wang"
 git config --global user.email "ixingo@126.com"
